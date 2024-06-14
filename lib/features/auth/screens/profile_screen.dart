39513bf6b0fa4f331fad798e00a179d096eab36f
Nuaiman Ashiq <ashiqnuaiman@.com>
2024-06-15 02:39:24 +0600

    commit

--- lib/features/auth/screens/profile_screen.dart
+++ lib/features/auth/screens/profile_screen.dart
@@ -1,10 +1,119 @@
+import 'package:bhokta_consumer/features/auth/widgets/auth_field.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class ProfileScreen extends StatelessWidget {
+import '../../../core/constants/palette.dart';
+
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final fullnameController = TextEditingController();
+  final fathernameController = TextEditingController();
+  final mothernameController = TextEditingController();
+  final professionController = TextEditingController();
+  final nidController = TextEditingController();
+  DateTime? birthDate;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(8.0),
+          child: CircleAvatar(
+            child: Icon(Icons.add),
+          ),
+        ),
+        title: const Text('প্রোফাইল'),
+        actions: [
+          ElevatedButton(
+            onPressed: () {},
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Palette.green,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+            ),
+            child: const Text(
+              'সংরক্ষণ',
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+          ),
+          const SizedBox(width: 16),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Column(
+          children: [
+            AuthField(controller: fullnameController, labelText: 'পুরো নাম'),
+            SizedBox(height: 8),
+            AuthField(controller: nidController, labelText: 'এনআইডি'),
+            SizedBox(height: 8),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'জন্ম তারিখ',
+                  style: TextStyle(fontSize: 18),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      border: Border.all()),
+                  child: ListTile(
+                    leading: Text(
+                      birthDate == null
+                          ? ''
+                          : DateFormat('dd/MM/yyy').format(birthDate!),
+                      style: const TextStyle(fontSize: 16),
+                    ),
+                    trailing: IconButton(
+                      onPressed: () async {
+                        final pickedDate = await showDatePicker(
+                          context: context,
+                          firstDate: DateTime(1990),
+                          lastDate: DateTime.now(),
+                        );
+                        if (pickedDate != null) {
+                          setState(() {
+                            birthDate = pickedDate;
+                          });
+                        }
+                      },
+                      icon: const Icon(Icons.calendar_month),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(height: 8),
+            AuthField(controller: professionController, labelText: 'পেশা'),
+            SizedBox(height: 8),
+            AuthField(controller: fathernameController, labelText: 'বাবার নাম'),
+            SizedBox(height: 8),
+            AuthField(
+                controller: mothernameController, labelText: 'মায়ের নাম'),
+            SizedBox(height: 8),
+            Divider(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton.icon(
+                  onPressed: () {},
+                  label: Text('পাসওয়ার্ড পরিবর্তন'),
+                  icon: Icon(Icons.lock_reset),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/features/records/screens/all_complains_screen.dart
+++ lib/features/records/screens/all_complains_screen.dart
@@ -5,6 +5,100 @@ class AllComplainsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('অভিযোগের তালিকা'),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: DataTable(
+            columns: const [
+              DataColumn(label: Text('ক্রমিক নং')),
+              DataColumn(label: Text('অভিযোগের ধরন')),
+              DataColumn(label: Text('অভিযোগ সংস্থার নাম')),
+              DataColumn(label: Text('অভিযোগের তারিখ')),
+              DataColumn(label: Text('অভিযোগের স্থিতি')),
+              DataColumn(label: Text('একশন')),
+            ],
+            rows: const [
+              DataRow(cells: [
+                DataCell(Text('1')),
+                DataCell(Text('Medicine')),
+                DataCell(Text('Ali Baba')),
+                DataCell(Text('01-05-2023')),
+                DataCell(Text('pending')),
+                DataCell(Text('বিস্তারিত')),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('1')),
+                DataCell(Text('Medicine')),
+                DataCell(Text('Ali Baba')),
+                DataCell(Text('01-05-2023')),
+                DataCell(Text('pending')),
+                DataCell(Text('বিস্তারিত')),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('1')),
+                DataCell(Text('Medicine')),
+                DataCell(Text('Ali Baba')),
+                DataCell(Text('01-05-2023')),
+                DataCell(Text('pending')),
+                DataCell(Text('বিস্তারিত')),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('1')),
+                DataCell(Text('Medicine')),
+                DataCell(Text('Ali Baba')),
+                DataCell(Text('01-05-2023')),
+                DataCell(Text('pending')),
+                DataCell(Text('বিস্তারিত')),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('1')),
+                DataCell(Text('Medicine')),
+                DataCell(Text('Ali Baba')),
+                DataCell(Text('01-05-2023')),
+                DataCell(Text('pending')),
+                DataCell(Text('বিস্তারিত')),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('1')),
+                DataCell(Text('Medicine')),
+                DataCell(Text('Ali Baba')),
+                DataCell(Text('01-05-2023')),
+                DataCell(Text('pending')),
+                DataCell(Text('বিস্তারিত')),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('1')),
+                DataCell(Text('Medicine')),
+                DataCell(Text('Ali Baba')),
+                DataCell(Text('01-05-2023')),
+                DataCell(Text('pending')),
+                DataCell(Text('বিস্তারিত')),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('1')),
+                DataCell(Text('Medicine')),
+                DataCell(Text('Ali Baba')),
+                DataCell(Text('01-05-2023')),
+                DataCell(Text('pending')),
+                DataCell(Text('বিস্তারিত')),
+              ]),
+              DataRow(cells: [
+                DataCell(Text('1')),
+                DataCell(Text('Medicine')),
+                DataCell(Text('Ali Baba')),
+                DataCell(Text('01-05-2023')),
+                DataCell(Text('pending')),
+                DataCell(Text('বিস্তারিত')),
+              ]),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
